284b2928eee24d277161075a58f95a199eed648b
Andrew Wang <spartan117aw@gmail.com>
2023-08-31 22:09:31 -0700

    main carousel scrolling done

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import './Carousel.css';
+import amazonRainforest from './img/amazonRainforest.jpg'
+import grandCanyon from './img/grandCanyon.jpg'
+import haLongBay from './img/haLongBay.jpg'
+import macchuPichu from './img/macchuPichu.jpg'
+import northernLights from './img/northernLights.jpg'
+import santorini from './img/santorini.jpg'
+import victoriaFalls from './img/victoriaFalls.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from 'react';
+
+const images = [grandCanyon,haLongBay,northernLights,santorini,macchuPichu,amazonRainforest, victoriaFalls]
+const displayNames = ["Grand Canyon", "Ha Long Bay", "Aurora Borealis", "Santorini", "Macchu Pichu", "Amazon Rainforest", "Victoria Falls"]
+const averageColors = ["#89807e", "#988b81", "#3b5778", "#788283","#766c67", "#55635c", "#98765a"]
+
+
+
+function Carousel() {
+
+    const mainTrack = document.getElementById('mainTrack');
+    
+
+    const [frozen, setFrozen] = useState(false);
+    const [hasMoved, setHasMoved] = useState(false);
+
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);//for mouse pos
+    const [offsetX, setOffsetX] = useState(0);//for carousel pos
+
+    const [percentOffsetX, setPercentOffsetX] = useState(0);
+    const [centeredImageIndex, setCenteredImageIndex] = useState(0);
+    
+    
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setHasMoved(false);
+        setStartX(e.clientX);
+    };
+
+    const handleMouseMove = (e) => {
+        if (!isDragging) return;
+        
+        setHasMoved(true);
+        let newOffsetX = e.clientX - startX;
+        const maxDelta = window.innerWidth/2;
+        let percent = (newOffsetX/maxDelta) * 100;
+        let maxScroll = 86.2;
+        percent += percentOffsetX;
+        percent = percent > 0 ? 0 : percent;
+        percent = percent < -maxScroll ? -maxScroll : percent;
+        setOffsetX(percent);
+        
+        animateImage();
+
+
+        let increment = 100/images.length;
+        const centeredIndex = Math.floor((-percent+8)/increment);
+        setCenteredImageIndex(centeredIndex);
+
+    };
+
+    const changeBackground = () => {
+
+        const background = document.querySelector('.backgroundBlurred');
+        const title = document.querySelector('.title');
+
+        background.style.backgroundImage = `url(${images[centeredImageIndex]})`;
+        background.style.opacity = 0.5;
+        title.style.opacity = 1;
+        title.style.left = "14vw";
+        title.style.color = centeredImageIndex===2 ? "white" : "white";
+        title.textContent = displayNames[centeredImageIndex];
+        
+
+
+    };
+
+    useEffect(() => {
+        // This effect will be triggered whenever 'centeredImageIndex' changes
+        const background = document.querySelector('.backgroundBlurred');
+        const title = document.querySelector('.title');
+        
+        background.style.opacity = 0;
+        title.style.opacity = 0;
+        title.style.left = "12vw";
+        const timer = setTimeout(() => {
+            changeBackground();
+        }, 500); 
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [centeredImageIndex]);
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+        setPercentOffsetX(offsetX);
+    };
+
+    const animateImage = () => {
+        
+        mainTrack.animate(
+            { transform: `translateX(${offsetX}%)` },
+            { duration: 1500, fill: 'forwards' }
+        );
+
+        const images = document.querySelectorAll('.carouselImage');
+        images.forEach(element =>{
+            element.animate(
+                { objectPosition: `${95+offsetX*1.1}% 50%` },
+                { duration: 1500, fill: 'forwards' }
+            );
+        });
+        
+    };
+
+    const handleImageEntry = (index) => {
+        if (!hasMoved){
+            console.log("click!", index)
+            setFrozen(true);
+            //mainTrack.children[index].style.position = "fixed";
+            mainTrack.children[index].style.left = 0;
+            mainTrack.children[index].style.top = 0;
+            mainTrack.children[index].style.width = "100vw";
+            mainTrack.children[index].style.height = "100vh";
+            mainTrack.children[index].style.zIndex = 3;
+        }
+
+    };
+    
+    return (
+        <div className = {`whole ${isDragging ? 'dragging' : ''}`} onMouseMove={handleMouseMove}
+             onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp}>
+            <div className="backgroundBlurred"/>
+            <div id = "mainTrack" onMouseDown={handleMouseDown} >
+                <img className="carouselImage" src = {grandCanyon} alt = "" draggable= "false" onMouseUp={(e) => handleImageEntry(0)}/>
+                <img className="carouselImage" src ={haLongBay} alt = "" draggable= "false" onMouseUp={(e) => handleImageEntry(1)}/>
+                <img className="carouselImage" src ={northernLights} alt = "" draggable= "false"onMouseUp={(e) => handleImageEntry(2)}/>
+                <img className="carouselImage" src ={santorini} alt = "" draggable= "false"onMouseUp={(e) => handleImageEntry(3)}/>
+                <img className="carouselImage" src ={macchuPichu} alt = "" draggable= "false"onMouseUp={(e) => handleImageEntry(4)}/>
+                <img className="carouselImage" src ={amazonRainforest} alt = "" draggable= "false"onMouseUp={(e) => handleImageEntry(5)}/>
+                <img className="carouselImage" src ={victoriaFalls} alt = "" draggable= "false"onMouseUp={(e) => handleImageEntry(6)}/>
+            </div>
+            
+            <div className="title"></div>
+            
+            
+        </div>
+        
+    
+    );
+    
+    
+
+
 }
 
-export default App;
+export default Carousel;
