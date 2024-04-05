00bcee1c68dedec68fd17c2276c52319178e7d58
wangaj4 <wangaj4@uci.edu>
2024-04-05 09:36:47 -0700

    nvm THIS is portfolio skeleton

--- src/Portfolioentry.js
+++ src/Portfolioentry.js
@@ -2,6 +2,8 @@ import './main.css';
 import './Portfolio.css';
 
 import React, {useState, useEffect, Fragment} from 'react';
+import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 let titles = ["Western Municipal","Glendale Water and Power","Cucamonga Valley Water","Loopy","Loopy2"]
 
@@ -14,6 +16,12 @@ function Portfolioentry(props){
     
     return (
         <Fragment>
+            <div className={"centerText"}>
+                <Nav.Link as = {Link} to = "/portfolio" className = "navbarItem">
+                    Back
+                </Nav.Link>
+            </div>
+            
             <div className={"titleBanner2"}>
                 <div className={"bannerTitle"} data-aos = "fade-up" data-aos-duration="800">
                     {titles[props.index]}
